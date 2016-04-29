074364925f3dcfd85e7dd4e2003ed96fee78951f
marcio.soares <marcio@mail.com>
2016-04-29 19:39:18 -0300

    adicionado package model e DAO;criado a classe de conexão;

---
+++ IMEG/src/main/java/DAO/Connection.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author marcio.soares <marcio@mail.com>
+ */
+public class Connection {
+    private java.sql.Connection obterConexao() throws SQLException, ClassNotFoundException {
+        java.sql.Connection conn = null;
+        // Passo 1: Registrar driver JDBC.
+        Class.forName("org.apache.derby.jdbc.ClientDataSource");
+
+        // Passo 2: Abrir a conexão
+        conn = DriverManager.getConnection(
+                "jdbc:derby://localhost:1527/agendabd;SecurityMechanism=3",
+                "app", // usuario
+                "app"); // senha
+        return conn;
+    }    
+}
