783c011721bc1d80ebd243ebc584c850e9584083
marcio.soares <marcio@mail.com>
2016-04-30 20:14:51 -0300

    reformulação do projeto

---
+++ IMEG/src/main/java/br/senac/tads/pi3/imeg/entity/Pessoa.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.imeg.entity;
+
+/**
+ *
+ * @author marcio.soares
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
