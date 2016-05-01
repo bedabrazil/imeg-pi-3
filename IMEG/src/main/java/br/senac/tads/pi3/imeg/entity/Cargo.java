9b88ca08c4389902f31386d40c97e2ca8ea76403
Eilane Santos <>
2016-05-01 19:15:08 -0300

    Pacote .classes renomeado para .entity

---
+++ IMEG/src/main/java/br/senac/tads/pi3/imeg/entity/Cargo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.imeg.entity;
+
+/**
+ *
+ * @author Eilane
+ */
+public class Cargo {
+    private int idcargo;
+    private String nomecargo;
+
+    public int getIdcargo() {
+        return idcargo;
+    }
+
+    public void setIdcargo(int idcargo) {
+        this.idcargo = idcargo;
+    }
+
+    public String getNomecargo() {
+        return nomecargo;
+    }
+
+    public void setNomecargo(String nomecargo) {
+        this.nomecargo = nomecargo;
+    }
+}
---
+++ IMEG/src/main/java/br/senac/tads/pi3/imeg/entity/Funcionario.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.imeg.entity;
+
+/**
+ *
+ * @author Eilane
+ */
+public class Funcionario {
+    private int idfuncionario;
+    private String nome;
+
+    public int getIdfuncionario() {
+        return idfuncionario;
+    }
+
+    public void setIdfuncionario(int idfuncionario) {
+        this.idfuncionario = idfuncionario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
