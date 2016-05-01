95ee16e69eb1d2d097bcdd9dbb766d463fe1c1fb
Eilane Santos <>
2016-05-01 19:55:18 -0300

    Criaçao da classe Estado

---
+++ IMEG/src/main/java/br/senac/tads/pi3/imeg/entity/Estado.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.imeg.entity;
+
+/**
+ *
+ * @author Amor
+ */
+public class Estado {
+    private int idestado;
+    private String nomeestado;
+
+    public int getIdestado() {
+        return idestado;
+    }
+
+    public String getNomeestado() {
+        return nomeestado;
+    }
+
+    public void setNomeestado(String nomeestado) {
+        this.nomeestado = nomeestado;
+    }
+}
