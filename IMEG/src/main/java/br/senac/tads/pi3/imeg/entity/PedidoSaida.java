487908e4feaba31378facc49afd237fa8fc363df
Eilane Santos <>
2016-05-01 21:46:52 -0300

    criacao classe pedidosaida

---
+++ IMEG/src/main/java/br/senac/tads/pi3/imeg/entity/PedidoSaida.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.imeg.entity;
+
+/**
+ *
+ * @author eilane
+ */
+public class PedidoSaida {
+    private int idsaida;
+    private int idproduto;
+    private int idfuncionario;
+
+    public int getIdsaida() {
+        return idsaida;
+    }
+
+    public int getIdproduto() {
+        return idproduto;
+    }
+
+    public int getIdfuncionario() {
+        return idfuncionario;
+    }
+}
