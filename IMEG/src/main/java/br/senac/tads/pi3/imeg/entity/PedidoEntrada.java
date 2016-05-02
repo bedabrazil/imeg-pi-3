85b76f0b771b000a5167f63ae723dc69e8616d62
Eilane Santos <>
2016-05-01 21:20:39 -0300

    Criacao da classe PedidoEntrada

---
+++ IMEG/src/main/java/br/senac/tads/pi3/imeg/entity/PedidoEntrada.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.imeg.entity;
+
+/**
+ *
+ * @author Amor
+ */
+public class PedidoEntrada {
+    private int identrada;
+    private int idproduto;
+    private int idfuncionario;
+
+    public int getIdentrada() {
+        return identrada;
+    }
+
+    public int getIdproduto() {
+        return idproduto;
+    }
+
+    public int getIdfuncionario() {
+        return idfuncionario;
+    }
+    
+}
