d62da1c3293d870839e12a06e66420137c3589f4
diogo.lsousa <diogo.lsousa@CAS485747W07C02.senacsp.edu.br>
2016-05-03 18:26:57 -0300

    Atualizando a Classe Categoria DAO

--- IMEG/src/main/java/br/senac/tads/pi3/imeg/dao/CategoriaDao.java
+++ IMEG/src/main/java/br/senac/tads/pi3/imeg/dao/CategoriaDao.java
@@ -7,7 +7,9 @@ package br.senac.tads.pi3.imeg.dao;
 
 import br.senac.tads.pi3.imeg.entity.Categoria;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,7 +18,9 @@ import java.util.logging.Logger;
  * @author diogo.lsousa
  */
 public class CategoriaDao {
+
     private PreparedStatement pst;
+
     public void incluirCategoria(Categoria categoria) {
 
         String sql = "INSERT INTO CATEGORIA(NOME, STATUS) VALUES(?,?)";
@@ -26,9 +30,8 @@ public class CategoriaDao {
             pst.setInt(1, categoria.getNome());
             pst.setBoolean(2, categoria.isStatus());
 
-            
-                pst.execute();
-            } catch (SQLException ex) {
+            pst.execute();
+        } catch (SQLException ex) {
             System.out.println("ERROR SQL: " + ex.getMessage());
         } finally {
             try {
@@ -37,22 +40,61 @@ public class CategoriaDao {
                 Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+    }
+
+    public ArrayList<Categoria> consultarCategoria(Categoria categoria) {
+
+        String sql = "SELECT * FROM CATEGORIA WHERE NOME LIKE '%" + categoria.getNome() + "%';";
+        ArrayList<Categoria> listaCategoria = new ArrayList<>();
+
+        try {
+            
+            pst = new Conexao().prepararStatement(sql);
+            ResultSet rs = pst.executeQuery(sql);
+
+            while (rs.next()) {
+                categoria.setNome(rs.getInt("NOME"));
+                categoria.setStatus(rs.getBoolean("STATUS"));
+
+                listaCategoria.add(categoria);
+            }
+            return listaCategoria;
+
+        } catch (SQLException ex) {
+            System.out.println("ERRO DE SQL: " + ex.getMessage());
+        } finally {
+            try {
+               pst.close();
+                
+            } catch (SQLException ex) {
+            }
         }
-    
-    
-    
-    public void consultarCategoria(Categoria categoria){
-    
+        return null;
     }
-    
-    public void alterarCategoria(Categoria categoria){
-    
+
+    public void alterarCategoria(Categoria categoria) {
+        String sql = "SELECT * FROM CATEGORIA WHERE ID ='"+ categoria.getId()+"'";
+        
+        try {       
+        pst= new Conexao().prepararStatement(sql);
+        ResultSet rs = pst.executeQuery(sql);
+        
+        pst.setInt(1, categoria.getNome());
+        pst.setBoolean(2, categoria.isStatus());
+              
+         } catch (SQLException ex) {
+            System.out.println("ERRO DE SQL: "+ex.getMessage());
+         }finally{
+            try {
+                pst.close();
+            } catch (SQLException ex) {
+            }
+        }
+
     }
-    
-    public void removerCategoria(Categoria categoria){
-    
+
+    public void removerCategoria(Categoria categoria) {
+
     }
-    
- 
 
 }
