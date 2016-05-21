c3a71e70fd4082213d67cd4e20d1bc2a78506fed
Eilane <Eilane@Eilane.Home>
2016-05-21 20:38:57 -0300

    Tela de vendas - Não concluido

---
+++ IMEG/src/main/java/br/senac/tads/pi3/imeg/servlet/VendaProdutoServlet.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.imeg.servlet;
+
+import br.senac.tads.pi3.imeg.dao.CategoriaDao;
+import br.senac.tads.pi3.imeg.dao.ProdutoDao;
+import br.senac.tads.pi3.imeg.entity.Categoria;
+import br.senac.tads.pi3.imeg.entity.ItemVenda;
+import br.senac.tads.pi3.imeg.entity.ListaItensVenda;
+import br.senac.tads.pi3.imeg.entity.Produto;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Eilane
+ */
+@WebServlet(name = "VendaProdutoServlet", urlPatterns = {"/produtos/venda"})
+public class VendaProdutoServlet extends HttpServlet {
+
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        ArrayList<Produto> produto = new ProdutoDao().listar();
+        request.setAttribute("produtos", produto);
+        
+        request.getRequestDispatcher("/WEB-INF/views/vendas/index.jsp").forward(request, response);
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+              
+        processRequest(request, response);
+    }
+
+ 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+        HttpSession session = request.getSession();
+        
+
+ 
+                int id=1;// Ajustar
+                Produto produto = new ProdutoDao().pesquisarPorId(id);
+                Categoria categoria = new CategoriaDao().pesquisarPorId(id); // Ajustar
+                
+                request.setAttribute("produto", produto);
+                
+                ItemVenda ItemProduto = new ItemVenda();
+                ItemProduto.setId(id);
+                ItemProduto.setProduto(produto);
+                ItemProduto.setCategoria(categoria);
+                ItemProduto.setQuantidade(Integer.parseInt(request.getParameter("quantidade")));
+                        
+                ListaItensVenda lista  = new ListaItensVenda();
+                
+                lista.adicionar(ItemProduto);
+                
+                processRequest(request, response);
+  
+        }
+        
+        
+  }
+
+
+
