f81651d4af6fd38d9e95e1c65c474c40a9eb4491
Mssouza <Mssouza@Priscila-PC>
2016-06-05 13:16:59 -0300

    dependencia de csv para exportar para excel pegando valores da DashBoard e tratando para converter para sqlDate

--- IMEG/src/main/java/br/senac/tads/pi3/imeg/servlet/DashBoardServlet.java
+++ IMEG/src/main/java/br/senac/tads/pi3/imeg/servlet/DashBoardServlet.java
@@ -5,11 +5,20 @@
  */
 package br.senac.tads.pi3.imeg.servlet;
 
+import au.com.bytecode.opencsv.CSVWriter;
 import br.senac.tads.pi3.imeg.dao.RelatorioDao;
 import br.senac.tads.pi3.imeg.entity.Funcionario;
 import br.senac.tads.pi3.imeg.entity.RelatorioVenda;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,13 +26,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
 /**
  *
  * @author Márcio Soares <marcio@mail.com>
  */
 @WebServlet(name = "DashBoardServlet", urlPatterns = {"/dashboard", "/painel"})
 public class DashBoardServlet extends HttpServlet {
-
 
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -42,11 +51,40 @@ public class DashBoardServlet extends HttpServlet {
         ArrayList<RelatorioVenda> maisVendidos = new RelatorioDao().listarMaisVendidos();
         request.setAttribute("maisVendidos", maisVendidos);
         request.getRequestDispatcher("/WEB-INF/views/home/bemvindo.jsp").forward(request, response);
-        
+
     }
-@Override
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.sendRedirect(request.getContextPath() + "/dashboard");
+        
+        ArrayList<String> mensagens = new ArrayList<>();
+
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+        if (!request.getParameter("date-ini-mais-vendidos").isEmpty()) {
+            mensagens.add("O campo data inicio não pode ser vazio.");
+        }
+        if (!request.getParameter("date-end-mais-vendidos").isEmpty()) {
+            mensagens.add("O campo data fim não pode ser vazio.");
+        }
+
+        String dt_inicio = (request.getParameter("date-ini-mais-vendidos"));
+        String dt_fim = (request.getParameter("date-end-mais-vendidos"));// recebe os valores de data em String
+
+        try {
+            java.sql.Date dataInicio = new java.sql.Date(format.parse(dt_inicio).getTime());
+            java.sql.Date dataFim = new java.sql.Date(format.parse(dt_fim).getTime());
+        } catch (ParseException ex) {
+            Logger.getLogger(DashBoardServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }// converte a data de String para sql do Date
+        
+        CSVWriter writer = new CSVWriter(new FileWriter("Relatorio.csv"), '\t');
+        
+        
+       
+       
+        writer.close();
     }
 }
